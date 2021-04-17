cb06648f26be1767b5749b6a846fa2a27b8b4e95
Gabriel <gabriel.estudy.reis@gmail.com>
2021-04-17 17:46:00 -0300

    att de respeito

--- AED1/7-3/6.c
+++ AED1/7-3/6.c
@@ -1,37 +1,39 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-void swap(char *v, int i, int j);
+void swap(char *frase, int i, int j);
 void heapify(char *frase, int size_frase, int i);
 void heapSort(char *frase, int size_frase);
 
 int main(){
 
-    char frase[24] = "Ordenando com Heap Sort";
+    char frase[24] = "Ordenando com heap Sort";
     printf("Antes: %s\n", frase);
-    heapSort(frase, 24);
+    heapSort(frase, 23);
 
-    printf("%s\n", frase);
+    printf("Depois: %s\n", frase);
     return 0;
 }
 
-void swap(char *v, int i, int j){
-
-    char temp = v[i];
-    v[i] = v[j];
-    v[j] = temp;
+void swap(char *frase, int i, int j){
+    //printf("Antes da swap %c e %c\n", i, j);
+    char temp = frase[i];
+    frase[i] = frase[j];
+    frase[j] = temp;
+    //printf("Depois %c e %c\n", i, j);
 }
 
 void heapify(char *frase, int size_frase, int i){
 
     int max_value = i;
-    int leftChild = 2 * i + 1;
-    int rightChild = 2 * i + 2;
+    int leftChild = 2 * i;
+    int rightChild = 2 * i + 1;
 
-    if (leftChild < size_frase && (int)(frase[leftChild]) > (int)(frase[max_value]))
+    if (leftChild < size_frase && (int)tolower(frase[leftChild]) < (int)tolower(frase[max_value]))
         max_value = leftChild;
 
-    if (rightChild < size_frase && (int)(frase[rightChild]) > (int)(frase[max_value]))
+    if (rightChild < size_frase && (int)tolower(frase[rightChild]) < (int)tolower(frase[max_value]))
         max_value = rightChild;
 
     if (max_value != i){
