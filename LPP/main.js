7868f925b0c07c5d28e0c36bf778def041d3e266
GabrielFRails <gabriel.estudy.reis@gmail.com>
2022-02-01 16:25:01 -0300

    add LPP folder and start new script for the next class

---
+++ LPP/main.js
@@ -0,0 +1,11 @@
+//Declaração de variáveis
+
+var a = 10;
+var b = "10";
+
+var c =  null;
+
+var resultado = a * b;
+console.log("resultado de 10 * '10' => ", resultado);
+
+console.log("isNaN(c) => ", isNaN(null));
--- main.js
+++
@@ -1,8 +0,0 @@
-function recursiva(num) {
-   if (num >= 10) return;
-   num++
-   recursiva(num)
-   console.log(num)
-}
-
-recursiva(0)
