2293c5b5ea30da4e29b73eb7f68e26d87d041834
Uzeda <gabrielsuzeda@gmail.com>
2022-02-03 18:25:50 -0300

    Adicionadas funcoes de manipulacao de string

--- LPP/main.js
+++ LPP/main.js
@@ -23,6 +23,25 @@ console.log("isNaN(true) => ", isNaN(d));
 console.log("isNaN(false) => ", isNaN(e));
 console.log("isNaN(undefined) => ", isNaN(f));
 
+
+//operações com strings
+var nome = "fulano";
+var sobrenome = "de tal";
+var nomeCompleto = nome + " " + sobrenome
+console.log("nomeCompleto: ", nomeCompleto) //concatenação
+console.log("nomeCompleto.length: ", nomeCompleto.length) //tamanho da string
+console.log("nomeCompleto.toUpperCase: ", nomeCompleto.toUpperCase()) //converte para maiúsculo
+console.log("nomeCompleto.toLowerCase: ", nomeCompleto.toLowerCase()) //converte para minúsculo
+console.log("nomeCompleto.charAt(0): ", nomeCompleto.charAt(0)) //pega o primeiro caractere
+console.log("nomeCompleto.charAt(nomeCompleto.length - 1): ", nomeCompleto.charAt(nomeCompleto.length - 1)) //pega o último caractere
+console.log("nomeCompleto.indexOf('a'): ", nomeCompleto.indexOf('a')) //pega a posição do caractere
+console.log("nomeCompleto.indexOf('a', 1): ", nomeCompleto.indexOf('a', 1)) //pega a posição do caractere a partir da posição 1
+console.log("nomeCompleto.substring(0, 3): ", nomeCompleto.substring(0, 3)) //pega a substring de 0 a 3
+console.log("nomeCompleto.substring(0, nomeCompleto.length - 1): ", nomeCompleto.substring(0, nomeCompleto.length - 1)) //pega a substring de 0 a nomeCompleto.length - 1
+console.log("nomeCompleto.substring(nomeCompleto.indexOf('a'), nomeCompleto.length): ", nomeCompleto.substring(nomeCompleto.indexOf('a'), nomeCompleto.length)) //pega a substring de nomeCompleto.indexOf('a') a nomeCompleto.length
+console.log("nomeCompleto.substring(nomeCompleto.indexOf('a'), nomeCompleto.indexOf('a') + 3): ", nomeCompleto.substring(nomeCompleto.indexOf('a'), nomeCompleto.indexOf('a') + 3)) //pega a substring de nomeCompleto.indexOf('a') a nomeCompleto.indexOf('a') + 3
+
+
 var g = {
     nome: "fulano",
     idade: 20
@@ -42,3 +61,5 @@ a = 'ABC'
 console.log("tipo de 'a':", typeof(a))
 //console.log(a.toFixed(5)) linha com erro pois toFixed é aplicável somente a números
 console.log(a.toLowerCase()) //método aplicável a strings
+
+
