57c2a9384cc06c35a952480330908858f4fc7963
GabrielFRails <gabriel.estudy.reis@gmail.com>
2022-02-17 10:15:07 -0300

    laboratorio parte 1

---
+++ SB/lab_07/2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int a = 0;
+
+long b = 0;
+
+int main()
+
+{
+
+   scanf("%d", &a); // utilize 'read_int_to_eax'
+
+   if (a)
+   {
+
+      scanf("%ld", &b); // utilize 'read_long_to_rax'
+
+      if (b < 5)
+      {
+
+         b = (a << 1) + (a * b);
+
+         printf("%ld\n", b); // utilize 'print_rax_as_long'
+      }
+
+      else
+      {
+
+         a = b ^ 0x1FL;
+
+         printf("0x%X\n", a); // utilize 'print_eax_as_hex'
+      }
+   }
+
+   return 0;
+}
